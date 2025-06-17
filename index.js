64babd92cd8ad581d0f4fc2b987767f6252076f8
André <lalbrob.genius@gmail.com>
2025-06-17 10:50:54 +0200

    referencias

---
+++ index.js
@@ -0,0 +1,4 @@
+ import{
+    refParrafo1,
+    refParrafo2
+} from "./funcions/referencia.js";
---
+++ referencias.js
@@ -0,0 +1,6 @@
+let refDiv1 = document.querySelector("#refDiv1");
+let refParrafo1 = refDiv1.parentElement.querySelector("#refParrafo1");
+let refParrafo2 = refDiv1.parentElement.querySelector("#refParrafo2");
+
+export{refParrafo1, refParrafo2, refDiv1};
+
